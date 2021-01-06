580241d60a0450e6d1911bb261f5f2c19b48b0ae
Ifeoma B. Nwobu <ifbnw@Ifeomas-MacBook-Air.local>
2021-01-06 20:27:38 +0100

    updated design

---
+++ calculator.js
@@ -0,0 +1,15 @@
+//add an event listener to click buttons
+//let numberButtons = document.getElementsByClassName('number');
+
+// document.getElementsById('one').addEventListener('click', function (event) {
+//     console.log(event)
+// });
+
+//console.log(document.getElementById('one').a)
+
+
+
+//create a function that collects the initial set of numbers
+// const firstNums = (x , y) => {
+
+// }
